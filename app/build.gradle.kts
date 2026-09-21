073beef45d22fba380bb4d9b13d9e54887c59016
gulshan <193488816+gulshan-bfrs03086@users.noreply.github.com>
2026-09-21 11:22:05 +0530

    Open 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
  */
 val versionMajor = 1
 val versionMinor = 5
-val versionPatch = 1
+val versionPatch = 2
 
 /**
  * The version code, derived from the version rather than tracked by hand.
